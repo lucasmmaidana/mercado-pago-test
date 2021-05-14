c8061e39d01f6220dc466953fc3209c1a558b00a
lucasmaidanadv <lucas.maidana@devlights.com>
2021-05-14 12:23:42 -0300

    Add Webhook route

---
+++ pages/api/webhook.js
@@ -0,0 +1,25 @@
+import Cors from "cors";
+import initMiddleware from "../../lib/init-middleware";
+
+const cors = initMiddleware(
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST", "OPTIONS"],
+  })
+);
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+    req.on("end", () => {
+      console.log(body, "webhook response");
+      res.end("ok");
+    });
+  }
+  return res.status(200);
+}
